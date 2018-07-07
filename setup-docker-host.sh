6ea53a423f551319448a1ae5eee7ad05b5771994
DarkTImes <darktimesareuponus@gmail.com>
2018-07-07 22:15:49 +0100

    Resolving typo

--- setup-docker-host.sh
+++ setup-docker-host.sh
@@ -1,6 +1,6 @@
 apt-get update
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh && rm get-docker.sh
-sapt-get install -qqy x11-xserver-utils xauth x11-apps
+apt-get install -qqy x11-xserver-utils xauth x11-apps
 xhost +local:docker
 reboot now
